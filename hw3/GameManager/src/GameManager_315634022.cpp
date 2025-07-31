510d0a3f063f66067a035c40fd0ecfe3dab6378e
Daniel Baruch <danielb534@gmail.com>
2025-07-31 14:18:33 +0300

    into print...

--- hw3/GameManager/src/GameManager_315634022.cpp
+++ hw3/GameManager/src/GameManager_315634022.cpp
@@ -139,7 +139,7 @@ void MyGameManager_315634022::prepareLog(const std::string& map_name, const std:
     if (!log_file_.is_open()) {
         ERROR_PRINT("LOGMANAGER", "prepareLog", "Failed to open log file: " + log_filename);
     } else {
-        DEBUG_PRINT("LOGMANAGER", "prepareLog", "Log file created successfully: " + log_filename);
+        INFO_PRINT("LOGMANAGER", "prepareLog", "Log file created successfully: " + log_filename);
     }
 }
 
